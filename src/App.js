10f98476c495d0d8e74a3902ce480f74beab0ea1
Dvooskid <dvooskid1234@gmail.com>
2024-08-21 16:51:51 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import "./App.css";
-import About from "./pages/about/About";
-import BoardOfDirectors from "./pages/team/BoardofDirectors";
-import Team from "./pages/team/Team";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Login from "./components/login/Login";
-import { Corporate, Flight, Travel, Vip, Visa } from "./pages/products/index";
-import Contact from "./components/contact/Contact";
+
+import Home from "./pages/home/Home";
+import Group from "./pages/group/Group";
+import BoardofDirectors from "./pages/team/BoardofDirectors";
+import Team from "./pages/team/Team";
+import Whatwedo from "./pages/whatwedo/Whatwedo";
+import OtherTeamMembers from "./pages/team/otherTeam";
 import BackgroundMusic from "./components/music/BackgroundMusic";
 import musicFile from "./assets/music/wonderful-world.mp3";
 import NotFound from "./pages/notfound/NotFound";
-import BookingResults from "./components/booking-form/bookingResults";
-import OtherTeamMembers from "./pages/team/otherTeam";
-import BookingForm from "./components/booking-form/BookingForm";
+import Career from "./pages/career/Career";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    AOS.init({
-      duration: 1500,
-      easing: "ease-in",
-      once: false,
-    });
-  }, []);
 
   useEffect(() => {
     const preloader = document.getElementById("preloader");
@@ -39,57 +30,31 @@ const App = () => {
     }, 2500);
   }, []);
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: false,
+    });
+  }, []);
   return (
-    <div className="App bg-light">
+    <div className="App">
       {loading ? (
         <div id="preloader" className="rotating-image"></div>
       ) : (
         <>
           <Navbar />
-         <BackgroundMusic src={musicFile} /> 
-          <div className="fade">
-            <Routes>
-              <Route index exact path="/" element={<Home />} />
-              <Route
-                index
-                exact
-                path="/book-flight"
-                element={<BookingForm />}
-              />
-              <Route
-                index
-                exact
-                path="/booking-summary"
-                element={<BookingResults />}
-              />
-              <Route index exact path="/get-started" element={<Login />} />
-              <Route exact path="/about-us" element={<About />} />
-              <Route
-                exact
-                path="/board-of-directors"
-                element={<BoardOfDirectors />}
-              />
-              <Route exact path="/management-team" element={<Team />} />
-              <Route
-                exact
-                path="/other-team-members"
-                element={<OtherTeamMembers />}
-              />
-
-              <Route exact path="/corporate-travels" element={<Corporate />} />
-              <Route
-                exact
-                path="/flight-reservation-ticketing"
-                element={<Flight />}
-              />
-              <Route exact path="/visa-assistant-services" element={<Visa />} />
-              <Route exact path="/travel-insurance" element={<Travel />} />
-              <Route exact path="/vip-travels" element={<Vip />} />
-              <Route exact path="/contact" element={<Contact />} />
-              <Route path="*" element={<NotFound />} />
-              {/* Catch-all route for 404 */}
-            </Routes>
-          </div>
+          <BackgroundMusic src={musicFile} />
+          <Routes>
+            <Route index exact path="/" element={<Home />} />
+            <Route path="/group" element={<Group />} />
+            <Route path="/our-services" element={<Whatwedo />} />
+            <Route path="/board-of-directors" element={<BoardofDirectors />} />
+            <Route path="/management-team" element={<Team />} />
+            <Route path="/other-team-members" element={<OtherTeamMembers />} />
+            <Route path="/career" element={<Career />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
           <Footer />
         </>
       )}
