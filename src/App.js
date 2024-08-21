11ef81c4a461099cd4cb82179213181c158e2727
Dvooskid <dvooskid1234@gmail.com>
2024-08-21 16:48:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
@@ -20,6 +20,8 @@ import OtherTeamMembers from "./pages/team/otherTeam";
 import BookingForm from "./components/booking-form/BookingForm";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     AOS.init({
       duration: 1500,
@@ -28,49 +30,69 @@ const App = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const preloader = document.getElementById("preloader");
+
+    setTimeout(() => {
+      preloader.style.display = "none";
+      setLoading(false);
+    }, 2500);
+  }, []);
+
   return (
     <div className="App bg-light">
-      <Navbar />
-      <BackgroundMusic src={musicFile} />
-      <div className="fade">
-        <Routes>
-          <Route index exact path="/" element={<Home />} />
-          <Route index exact path="/book-flight" element={<BookingForm />} />
-          <Route
-            index
-            exact
-            path="/booking-summary"
-            element={<BookingResults />}
-          />
-          <Route index exact path="/get-started" element={<Login />} />
-          <Route exact path="/about-us" element={<About />} />
-          <Route
-            exact
-            path="/board-of-directors"
-            element={<BoardOfDirectors />}
-          />
-          <Route exact path="/management-team" element={<Team />} />
-          <Route
-            exact
-            path="/other-team-members"
-            element={<OtherTeamMembers />}
-          />
+      {loading ? (
+        <div id="preloader" className="rotating-image"></div>
+      ) : (
+        <>
+          <Navbar />
+         <BackgroundMusic src={musicFile} /> 
+          <div className="fade">
+            <Routes>
+              <Route index exact path="/" element={<Home />} />
+              <Route
+                index
+                exact
+                path="/book-flight"
+                element={<BookingForm />}
+              />
+              <Route
+                index
+                exact
+                path="/booking-summary"
+                element={<BookingResults />}
+              />
+              <Route index exact path="/get-started" element={<Login />} />
+              <Route exact path="/about-us" element={<About />} />
+              <Route
+                exact
+                path="/board-of-directors"
+                element={<BoardOfDirectors />}
+              />
+              <Route exact path="/management-team" element={<Team />} />
+              <Route
+                exact
+                path="/other-team-members"
+                element={<OtherTeamMembers />}
+              />
 
-          <Route exact path="/corporate-travels" element={<Corporate />} />
-          <Route
-            exact
-            path="/flight-reservation-ticketing"
-            element={<Flight />}
-          />
-          <Route exact path="/visa-assistant-services" element={<Visa />} />
-          <Route exact path="/travel-insurance" element={<Travel />} />
-          <Route exact path="/vip-travels" element={<Vip />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-          {/* Catch-all route for 404 */}
-        </Routes>
-      </div>
-      <Footer />
+              <Route exact path="/corporate-travels" element={<Corporate />} />
+              <Route
+                exact
+                path="/flight-reservation-ticketing"
+                element={<Flight />}
+              />
+              <Route exact path="/visa-assistant-services" element={<Visa />} />
+              <Route exact path="/travel-insurance" element={<Travel />} />
+              <Route exact path="/vip-travels" element={<Vip />} />
+              <Route exact path="/contact" element={<Contact />} />
+              <Route path="*" element={<NotFound />} />
+              {/* Catch-all route for 404 */}
+            </Routes>
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
