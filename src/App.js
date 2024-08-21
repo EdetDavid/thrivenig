63f0451b8cd76b33384bb99064a4326db407a6d4
Dvooskid <dvooskid1234@gmail.com>
2024-08-21 16:59:14 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,63 +1,88 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import "./App.css";
-
+import About from "./pages/about/About";
+import BoardOfDirectors from "./pages/team/BoardofDirectors";
+import Team from "./pages/team/Team";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-import Home from "./pages/home/Home";
-import Group from "./pages/group/Group";
-import BoardofDirectors from "./pages/team/BoardofDirectors";
-import Team from "./pages/team/Team";
-import Whatwedo from "./pages/whatwedo/Whatwedo";
-import OtherTeamMembers from "./pages/team/otherTeam";
+import Login from "./components/login/Login";
+import { Corporate, Flight, Travel, Vip, Visa } from "./pages/products/index";
+import Contact from "./components/contact/Contact";
 import BackgroundMusic from "./components/music/BackgroundMusic";
 import musicFile from "./assets/music/wonderful-world.mp3";
 import NotFound from "./pages/notfound/NotFound";
-import Career from "./pages/career/Career";
+import BookingResults from "./components/booking-form/bookingResults";
+import OtherTeamMembers from "./pages/team/otherTeam";
+import BookingForm from "./components/booking-form/BookingForm";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const preloader = document.getElementById("preloader");
-
-    setTimeout(() => {
-      preloader.style.display = "none";
-      setLoading(false);
-    }, 2500);
-  }, []);
+  
 
   useEffect(() => {
     AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
+      duration: 1500,
+      easing: "ease-in",
       once: false,
     });
   }, []);
+
+
+
   return (
-    <div className="App">
-      {loading ? (
-        <div id="preloader" className="rotating-image"></div>
-      ) : (
-        <>
+    <div className="App bg-light">
+   
           <Navbar />
-          <BackgroundMusic src={musicFile} />
-          <Routes>
-            <Route index exact path="/" element={<Home />} />
-            <Route path="/group" element={<Group />} />
-            <Route path="/our-services" element={<Whatwedo />} />
-            <Route path="/board-of-directors" element={<BoardofDirectors />} />
-            <Route path="/management-team" element={<Team />} />
-            <Route path="/other-team-members" element={<OtherTeamMembers />} />
-            <Route path="/career" element={<Career />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+         <BackgroundMusic src={musicFile} /> 
+          <div className="fade">
+            <Routes>
+              <Route index exact path="/" element={<Home />} />
+              <Route
+                index
+                exact
+                path="/book-flight"
+                element={<BookingForm />}
+              />
+              <Route
+                index
+                exact
+                path="/booking-summary"
+                element={<BookingResults />}
+              />
+              <Route index exact path="/get-started" element={<Login />} />
+              <Route exact path="/about-us" element={<About />} />
+              <Route
+                exact
+                path="/board-of-directors"
+                element={<BoardOfDirectors />}
+              />
+              <Route exact path="/management-team" element={<Team />} />
+              <Route
+                exact
+                path="/other-team-members"
+                element={<OtherTeamMembers />}
+              />
+
+              <Route exact path="/corporate-travels" element={<Corporate />} />
+              <Route
+                exact
+                path="/flight-reservation-ticketing"
+                element={<Flight />}
+              />
+              <Route exact path="/visa-assistant-services" element={<Visa />} />
+              <Route exact path="/travel-insurance" element={<Travel />} />
+              <Route exact path="/vip-travels" element={<Vip />} />
+              <Route exact path="/contact" element={<Contact />} />
+              <Route path="*" element={<NotFound />} />
+              {/* Catch-all route for 404 */}
+            </Routes>
+          </div>
           <Footer />
-        </>
-      )}
+        
+      
     </div>
   );
 };
