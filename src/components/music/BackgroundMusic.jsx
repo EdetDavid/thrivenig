6340a1ee0390cfa7861192a955ce5fbe835432a1
Dvooskid <dvooskid1234@gmail.com>
2026-01-08 14:04:58 +0100

    Update on Background Music Component

--- src/components/music/BackgroundMusic.jsx
+++ src/components/music/BackgroundMusic.jsx
@@ -15,21 +15,12 @@ const BackgroundMusic = ({ src }) => {
     }
   };
 
-  const stopMusic = () => {
-    audioRef.current.pause();
-    audioRef.current.currentTime = 0;
-    setIsPlaying(false);
-  };
-
   return (
     <div>
       <audio ref={audioRef} src={src} autoPlay loop />
       <div id="controls" className="controls">
-        <button onClick={togglePlay} className="control-button">
-          {isPlaying ? <i className="fas fa-pause"></i> : <i className="fas fa-play"></i>}
-        </button>
-        <button onClick={stopMusic} className="control-button">
-          <i className="fas fa-stop"></i>
+        <button onClick={togglePlay} className={`control-button ${isPlaying ? 'playing' : ''}`}>
+          <i className="fas fa-music"></i>
         </button>
       </div>
     </div>
